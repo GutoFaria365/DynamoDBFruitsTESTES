760054c85e778f22e72cc8d74ba1bcd1cd1894a1
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-03-27 12:22:32 +0100

    useful commands added

--- src/main/java/org/acme/dynamodb/AbstractService.java
+++ src/main/java/org/acme/dynamodb/AbstractService.java
@@ -20,12 +20,12 @@ public abstract class AbstractService {
 
     protected PutItemRequest putRequest(Fruit fruit) {
         Map<String, AttributeValue> item = new HashMap<>();
-        item.put("PK", AttributeValue.builder().s("FRUIT#" + fruit.getName()).build());
-        item.put("SK", AttributeValue.builder().s("TASTE#" + fruit.getDescription()).build());
-        item.put(FRUIT_NAME_COL, AttributeValue.builder().s(fruit.getName()).build());
-        item.put(FRUIT_DESC_COL, AttributeValue.builder().s(fruit.getDescription()).build());
-        item.put("GSI1PK", AttributeValue.builder().s("CATEGORY#" + fruit.getName()).build());
-        item.put("GSI1SK", AttributeValue.builder().s("CATEGORY#" + fruit.getDescription()).build());
+        item.put("PK", AttributeValue.builder().s("FRUIT#").build());
+        item.put("SK", AttributeValue.builder().s("FRUIT#" + fruit.getName().toUpperCase()).build());
+        item.put(FRUIT_NAME_COL, AttributeValue.builder().s(fruit.getName().toUpperCase()).build());
+        item.put(FRUIT_DESC_COL, AttributeValue.builder().s(fruit.getDescription().toUpperCase()).build());
+        item.put("GSI1PK", AttributeValue.builder().s("TASTE#").build());
+        item.put("GSI1SK", AttributeValue.builder().s("TASTE#" + fruit.getDescription().toUpperCase()).build());
 
         return PutItemRequest.builder()
                 .tableName(getTableName())
